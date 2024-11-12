450e3a30788ec28b1531d6aaf012876c7be8cd34
Taciano <taciano3003@gmail.com>
2024-11-12 16:09:41 -0300

    unificacion de auth y terminos y condiciones

--- src/app/terminos-y-condiciones/layout.tsx
+++ src/app/terminos-y-condiciones/layout.tsx
@@ -1,24 +1,24 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
+import "../globals.css";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "../fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+  src: "../fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
 
 export const metadata: Metadata = {
-  title: "Autenticacion",
-  description: "Vista de registro y log in",
+  title: "Terminos y condiciones",
+  description: "Vista de terminos y condiciones de la app",
 };
 
-export default function AuthLayout({
+export default function TermsAndConditionsLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
